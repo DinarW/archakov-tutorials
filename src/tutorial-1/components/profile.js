4927f3e2e006561bcbb5d5d88b1dd26f6261f88c
dinar <dinarmkbg@gmail.com>
2021-12-10 18:13:55 +0300

    fix destruct

--- src/tutorial-1/components/profile.js
+++ src/tutorial-1/components/profile.js
@@ -1,10 +1,10 @@
 import parseDate from "./parseDate";
 
-function Profile(props) {
-  const correctDate = parseDate(props.registredAt);
+function Profile({ name, registredAt }) {
+  const correctDate = parseDate(registredAt);
   return (
     <div className='content'>
-      <h2>Привет, <b>{props.name}!</b></h2>
+      <h2>Привет, <b>{name}!</b></h2>
       <p>Дата регистрации: {correctDate}</p>
     </div>
   )
