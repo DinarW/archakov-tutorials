1bd127c53eb68ddff974e72d9aea5c9122eec8dc
dinar <dinarmkbg@gmail.com>
2021-12-13 20:51:21 +0300

    change click event

--- src/tutorial-3/App.js
+++ src/tutorial-3/App.js
@@ -1,14 +1,20 @@
 const tasks = ['Изучить первый блок Reac Js'];
 
 const handleClickRemove = (event) => {
-  const index = event.target.dataset.taskId;
-  tasks.splice(index, 1);
-  render();
+  if (event.target.type === 'submit') {
+    const index = event.target.dataset.taskId;
+    tasks.splice(index, 1);
+    render();
+  }
 };
 
 const handleClickAdd = () => {
   const input = document.querySelector('input');
   const { value } = input;
+  if (!value) {
+    alert('Введите текст задачи!');
+    return;
+  }
   tasks.push(value);
   render();
   input.value = '';
@@ -17,22 +23,16 @@ const handleClickAdd = () => {
 const render = () => {
   // помещаем внутрь ul список задач из массива tasks
   const ul = document.querySelector('ul');
-  ul.innerHTML = '';
   const ulContent = tasks
-    .map((task, index) => `<li>${task} <button data-task-id=${index}>X</button>`)
+    .map((task, index) => `<li>${task} <button data-task-id=${index}>X</button></li>`)
     .join('\n');  
-  ul.innerHTML = ulContent;
-
-  // навешиваем на каждую кнопку задачи событие удаления из списка
-  const liTasks = [...ul.querySelectorAll('li')];
-  liTasks.forEach((li) => {
-    const button = li.querySelector('button');
-    button.addEventListener('click', handleClickRemove);
-  });
+  ul.innerHTML = ulContent;  
 };
 
 // рендерим задачи после загрузки/перезагрузки страницы
 window.onload = render;
-
+// вешаем событие клика на кнопку и список
 const addButton = document.querySelector('.addTask');
+const ul = document.querySelector('ul');
 addButton.addEventListener('click', handleClickAdd);
+ul.addEventListener('click', handleClickRemove);
